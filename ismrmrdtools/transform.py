fixing scaling in transform.py

ismrmrdtools/transform.py
@@ -1,7 +1,6 @@
 import numpy as np
 from numpy.fft import fftshift, ifftshift, fftn, ifftn
 
-
 def transform_kspace_to_image(k, dim=None, img_shape=None):
     """ Computes the Fourier transform from k-space to image space
     along a given or all dimensions
@@ -15,7 +14,7 @@ def transform_kspace_to_image(k, dim=None, img_shape=None):
         dim = range(k.ndim)
 
     img = fftshift(ifftn(ifftshift(k, axes=dim), s=img_shape, axes=dim), axes=dim)
-    img *= np.sqrt(np.prod(img.shape))
+    img *= np.sqrt(np.prod(np.take(img.shape, dim)))
     return img
 
 
@@ -32,5 +31,5 @@ def transform_image_to_kspace(img, dim=None, k_shape=None):
         dim = range(img.ndim)
 
     k = fftshift(fft(ifftshift(img, axes=dim), s=k_shape, axes=dim), axes=dim)
-    k /= np.sqrt(np.prod(img.shape))
+    k /= np.sqrt(np.prod(np.take(img.shape, dim)))
     return k