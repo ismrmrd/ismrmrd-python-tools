54b942312981f7b7ec613d241ef490a2a9202755
Souheil Inati <souheil.inati@nih.gov>
2015-01-29 12:29:22 -0500

    Added simple example to reconstruct from an ismrmrd dataset

---
+++ recon_ismrmrd_dataset.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+
+import os
+import ismrmrd
+import ismrmrd.xsd
+import ismrmrdtools as it
+import numpy as np
+
+# Load file
+filename = '/tmp/testdata.h5'
+if not os.path.isfile(filename):
+    print("%s is not a valid file" % filename)
+    raise SystemExit
+dset = ismrmrd.Dataset(filename, 'dataset', create_if_needed=False)
+
+header = ismrmrd.xsd.CreateFromDocument(dset.read_xml_header())
+enc = header.encoding[0]
+
+# Matrix size
+eNx = enc.encodedSpace.matrixSize.x
+eNy = enc.encodedSpace.matrixSize.y
+eNz = enc.encodedSpace.matrixSize.z
+rNx = enc.reconSpace.matrixSize.x
+rNy = enc.reconSpace.matrixSize.y
+rNz = enc.reconSpace.matrixSize.z
+
+# Field of View
+eFOVx = enc.encodedSpace.fieldOfView_mm.x
+eFOVy = enc.encodedSpace.fieldOfView_mm.y
+eFOVz = enc.encodedSpace.fieldOfView_mm.z
+rFOVx = enc.reconSpace.fieldOfView_mm.x
+rFOVy = enc.reconSpace.fieldOfView_mm.y
+rFOVz = enc.reconSpace.fieldOfView_mm.z
+
+# Number of Slices, Reps, Contrasts, etc.
+ncoils = header.acquisitionSystemInformation.receiverChannels
+if enc.encodingLimits.slice != None:
+    nslices = enc.encodingLimits.slice.maximum + 1
+else:
+    nslices = 1
+
+if enc.encodingLimits.repetition != None:
+    nreps = enc.encodingLimits.repetition.maximum + 1
+else:
+    nreps = 1
+
+if enc.encodingLimits.contrast != None:
+    ncontrasts = enc.encodingLimits.contrast.maximum + 1
+else:
+    ncontrasts = 1
+
+# TODO loop through the acquisitions looking for noise scans
+firstacq=0
+for acqnum in range(dset.number_of_acquisitions):
+    acq = dset.read_acquisition(acqnum)
+    
+    # TODO: Currently ignoring noise scans
+    if acq.flags & ismrmrd.ACQ_IS_NOISE_MEASUREMENT:
+        continue
+    else:
+        firstacq = acqnum
+        break
+
+
+# Initialiaze a storage array
+all_data = np.zeros((nreps, ncontrasts, nslices, ncoils, eNz, eNy, rNx), dtype=np.complex64)
+
+# Loop through the rest of the acquisitions and stuff
+for acqnum in range(firstacq,dset.number_of_acquisitions):
+    acq = dset.read_acquisition(acqnum)
+
+    # TODO: this is where we would apply noise pre-whitening
+
+    # Remove oversampling if needed
+    if eNx != rNx:
+        xline = it.transform_kspace_to_image(acq.data, [1])
+        x0 = (eNx - rNx) / 2
+        x1 = (eNx - rNx) / 2 + rNx
+        xline = xline[:,x0:x1]
+        acq.resize(rNx,acq.active_channels,acq.trajectory_dimensions)
+        acq.center_sample = rNx/2
+        # need to use the [:] notation here to fill the data
+        acq.data[:] = it.transform_image_to_kspace(xline, [1])
+  
+    # Stuff into the buffer
+    rep = acq.idx.repetition
+    contrast = acq.idx.contrast
+    slice = acq.idx.slice
+    y = acq.idx.kspace_encode_step_1
+    z = acq.idx.kspace_encode_step_2
+    all_data[rep, contrast, slice, :, z, y, :] = acq.data
+
+# Reconstruct images
+images = np.zeros((nreps, ncontrasts, nslices, eNz, eNy, rNx), dtype=np.float32)
+for rep in range(nreps):
+    for contrast in range(ncontrasts):
+        for slice in range(nslices):
+            # FFT
+            if eNz>1:
+                #3D
+                im = it.transform_kspace_to_image(all_data[rep,contrast,slice,:,:,:,:], [1,2,3])
+            else:
+                #2D
+                im = it.transform_kspace_to_image(all_data[rep,contrast,slice,:,0,:,:], [1,2])
+
+            # Sum of squares
+            im = np.sqrt(np.sum(np.abs(im) ** 2, 0))
+            
+            # Stuff into the output
+            if eNz>1:
+                #3D
+                images[rep,contrast,slice,:,:,:] = im
+            else:
+                #2D
+                images[rep,contrast,slice,0,:,:] = im
+
+# Show an image
+it.show.imshow(np.squeeze(images[0,0,0,:,:,:]))
