4a71c42e82021d78916c75cf8bb9a520dfa884a6
Michael S. Hansen <michael.hansen@nih.gov>
2015-01-31 12:56:14 -0500

    Working GRAPPA with regularization fixed

--- ismrmrdtools/grappa.py
+++ ismrmrdtools/grappa.py
@@ -1,11 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
 
-from numpy.fft import fftshift, ifftshift, fftn, ifftn
+from numpy.fft import fftshift, ifftshift,ifftn
 def calculate_grappa_unmixing(source_data, acc_factor, kernel_size=(4,5), data_mask=None, csm=None, target_data=None):
     
-    nx = source_data.shape[2]
     ny = source_data.shape[1]
     nc_source = source_data.shape[0]
 
@@ -16,11 +14,11 @@ def calculate_grappa_unmixing(source_data, acc_factor, kernel_size=(4,5), data_m
     if data_mask==None:
         data_mask = np.ones((ny, nc_source))
         
+    nc_target = target_data.shape[0]
+        
     if csm==None:
         print "Error, no CSM supped"
         return 0
-        
-    nc_target = target_data.shape[0]
         
 #%If csm is not provided, we will estimate it.
 #if (isempty(csm)),
@@ -67,21 +65,12 @@ def calculate_grappa_unmixing(source_data, acc_factor, kernel_size=(4,5), data_m
         kernel_pad = fftshift(ifftn(ifftshift(kernel_pad, axes=(1,2)), axes=(1,2)), axes=(1,2))
         kernel_pad *= unmix.shape[1]*unmix.shape[2]
         unmix = unmix + (kernel_pad * np.tile(np.conj(csm[c,:,:]) /csm_ss,(nc_source,1,1)))
-#
-#%Loop over target coils and fo b1-weighted combination in image space.
-#csm_ss = sum(conj(csm).*csm,3);
-#csm_ss(csm_ss < eps) = 1; %Avoid devision by zeros where coils are undefined
-#for c=1:target_coils,
-#    kernel_pad = pad_grappa_kernel(kernel(:,:,:,c),size(target_data));
-#    kernel_pad = fftshift(ifft(ifftshift(kernel_pad,1),[],1),1);
-#    kernel_pad = fftshift(ifft(ifftshift(kernel_pad,2),[],2),2);
-#    kernel_pad = kernel_pad*(size(kernel_pad,1)*size(kernel_pad,2));
-#    unmix = unmix + (kernel_pad .* repmat(conj(csm(:,:,c)) ./csm_ss,[1 1 coils]));
-#end
-#
-#unmix = unmix/acc_factor;    
+
+    unmix /= acc_factor
+    gmap = np.squeeze(np.sqrt(np.sum(abs(unmix) ** 2, 0))) * np.squeeze(np.sqrt(np.sum(abs(csm) ** 2, 0)))
     
-    return unmix
+    
+    return (unmix,gmap)
     
 def estimate_convolution_kernel(source_data, kernel_mask, target_data=None):
 
@@ -121,7 +110,7 @@ def estimate_convolution_kernel(source_data, kernel_mask, target_data=None):
     
 
     S = np.linalg.svd(A,compute_uv=False)
-    A_inv = np.dot(np.linalg.pinv(np.dot(A.H,A) + np.eye(A.shape[1])*(1e-6*np.max(abs(S))**2)),A.H)
+    A_inv = np.dot(np.linalg.pinv(np.dot(A.H,A) + np.eye(A.shape[1])*(1e-3*np.max(np.abs(S)))**2),A.H)
     x = np.dot(A_inv,b)
     
     offsets = np.argwhere(kernel_mask==1)
@@ -141,163 +130,3 @@ def pad_kernel(gkernel,padded_shape):
     padded_kernel[padding_before[0]:(padding_before[0]+gkernel.shape[0]),padding_before[1]:(padding_before[1]+gkernel.shape[1]),padding_before[2]:(padding_before[2]+gkernel.shape[2])] = gkernel
     return padded_kernel
      
-#function [unmix, gmap] = ismrm_calculate_grappa_unmixing(source_data, kernel_size, acc_factor, data_mask, csm, target_data, verbose)
-#%
-#%   [unmix, gmap] = ismrm_calculate_grappa_unmixing(source_data, kernel_size, acc_factor, data_mask, csm, target_data, verbose)
-#%   
-#%   Calculates b1-weighted image space GRAPPA unmixing coefficients.
-#%
-#%   INPUT:
-#%       source_data [kx,ky,coil]   : Source data for grappa kernel estimation (k-space)
-#%       kernel_size [kx,ky]        : e.g. [4 5]
-#%       acc_factor  scalar         : Acceleration factor, e.g. 2
-#%       data_mask   [kx,ky]        : '1' = calibration data, '0' = ignore
-#%       csm         [x,y,c]        : Coil sensitivity map, if empty, it
-#%                                    will be estimated from the reference lines.
-#%       target_data [kx,ky,coil]   : Target coil data, defaults to source data
-#%       verbose     bool           : Set true for verbose output
-#%
-#%   OUTPUT:
-#%       unmix [x,y,coil]           : Image unmixing coefficients
-#%       gmap  [x, y]               : Noise enhancement map 
-#%
-#%   Typical usage:
-#%       [unmix] = calculate_grappa_unmixing(source_data, [5 4], 4);
-#%
-#%
-#%   Notes:
-#%     - The unmixing coefficients produced by this routine produce uniform 
-#%       noise distribution images when there is no acceleration, i.e. the
-#%       noise in each pixel will be input noise * g-factor, where g-factor
-#%       is sqrt(sum(abs(unmix).^2,3)).
-#%
-#%       If you have coil sensitivities where the RSS of the coil
-#%       sensitivites is not 1 in each pixel, e.g. as obtained with a
-#%       seperate calibration scan using a body coil, and you would like a
-#%       uniform sensitivity image. You must apply that weighting after the
-#%       parallel imaging reconstruction by dividin with the RSS of the coil
-#%       sensitivites. 
-#%
-#%   Code made available for the ISMRM 2013 Sunrise Educational Course
-#% 
-#%   Michael S. Hansen (michael.hansen@nih.gov)
-#%
-#
-#if nargin < 3,
-#   error('At least 4 arguments needed'); 
-#end
-#
-#if nargin < 4,
-#    data_mask = [];
-#end
-#
-#if nargin < 5,
-#    csm = [];
-#end
-#
-#if nargin < 6,
-#    target_data = [];
-#end
-#
-#if nargin < 7,
-#    verbose = false;
-#end
-#
-#if (isempty(target_data)),
-#        target_data = source_data;
-#end
-#
-#
-#if (isempty(data_mask)),
-#    data_mask = ones(size(source_data,1),size(source_data,2));
-#end
-#
-#if (length(size(source_data)) == 2),
-#    coils = 1;
-#else
-#    coils = size(source_data,length(size(source_data)));
-#end
-#
-#if (length(size(target_data)) == 2),
-#    target_coils = 1;
-#else
-#    target_coils = size(target_data,length(size(target_data)));
-#end
-#
-#%If csm is not provided, we will estimate it.
-#if (isempty(csm)),
-#    if (verbose),
-#        fprintf('Estimating coil sensitivity...');
-#    end
-#    %Apply some filtering to avoid ringing
-#    f = hamming(max(sum(data_mask,1))) * hamming(max(sum(data_mask,2)))';
-#    fmask = zeros(size(source_data));
-#    fmask((1:size(f,1))+bitshift(size(source_data,1),-1)-bitshift(size(f,1),-1), ...
-#          (1:size(f,2))+bitshift(size(source_data,2),-1)-bitshift(size(f,2),-1), :) = ...
-#          repmat(f, [1 1 size(source_data,3)]);
-#    csm = ismrm_transform_kspace_to_image(source_data .* fmask, [1 2]);
-#    csm = ismrm_estimate_csm_walsh(csm); %Estimate coil sensitivity maps.
-#    if (verbose),
-#        fprintf('done.\n');
-#    end
-#end
-#    
-#
-#kernel = zeros(kernel_size(1),kernel_size(2)*acc_factor,coils,target_coils);
-#
-#if (verbose),
-#    fprintf('Calculating grappa kernels...\n');
-#end
-#
-#[kx_cal,ky_cal] = ind2sub(size(data_mask),[find(data_mask == 1,1,'first') find(data_mask == 1,1,'last')]);
-#for s=1:acc_factor,
-#   kernel_mask = zeros(kernel_size(1),kernel_size(2)*acc_factor);
-#   kernel_mask(:,s:acc_factor:end) = 1;
-#   k = ismrm_estimate_convolution_kernel(source_data(kx_cal(1):kx_cal(2),ky_cal(1):ky_cal(2),:),kernel_mask,target_data(kx_cal(1):kx_cal(2),ky_cal(1):ky_cal(2),:));
-#   kernel = kernel + k;
-#end
-#
-#
-#
-#kernel = flipdim(flipdim(kernel,1),2); %Flip dimensions in preparation for convolution.
-#
-#unmix = zeros(size(source_data));
-#if (nargout > 2),
-#   unmix_sc = zeros(size(unmix,1),size(unmix,2),coils,coils); 
-#end
-#
-#if (verbose),
-#    fprintf('Doing B1 weighted combination....');
-#end
-#
-#%Loop over target coils and fo b1-weighted combination in image space.
-#csm_ss = sum(conj(csm).*csm,3);
-#csm_ss(csm_ss < eps) = 1; %Avoid devision by zeros where coils are undefined
-#for c=1:target_coils,
-#    kernel_pad = pad_grappa_kernel(kernel(:,:,:,c),size(target_data));
-#    kernel_pad = fftshift(ifft(ifftshift(kernel_pad,1),[],1),1);
-#    kernel_pad = fftshift(ifft(ifftshift(kernel_pad,2),[],2),2);
-#    kernel_pad = kernel_pad*(size(kernel_pad,1)*size(kernel_pad,2));
-#    unmix = unmix + (kernel_pad .* repmat(conj(csm(:,:,c)) ./csm_ss,[1 1 coils]));
-#end
-#
-#unmix = unmix/acc_factor;
-#
-#if (nargout > 1),
-#   gmap = sqrt(sum(abs(unmix).^2,3)) .* sqrt(sum(abs(csm).^2,3));
-#end
-#
-#
-#if (verbose),
-#    fprintf('done.\n');
-#end
-#
-#return
-#
-#%Utility function for padding grappa kernel
-#function padded_kernel = pad_grappa_kernel(gkernel, image_size)
-#    padded_kernel = zeros(image_size(1),image_size(2),size(gkernel,3));
-#    padded_kernel([1:size(gkernel,1)]+bitshift(image_size(1)-size(gkernel,1),-1)+1, ...
-#        [1:size(gkernel,2)]+bitshift(image_size(2)-size(gkernel,2),-1)+1, :) = gkernel;
-#return
-#        
--- parallel_imaging_demo.py
+++ parallel_imaging_demo.py
@@ -10,44 +10,37 @@ from ismrmrdtools import simulation
 from ismrmrdtools import transform
 from ismrmrdtools import grappa
 
-##%%
-#reload(simulation)
-#matrix_size = 256
-#csm = simulation.generate_birdcage_sensitivities(matrix_size)
-#phan = simulation.phantom(matrix_size)
-#coil_images = np.tile(phan,(8, 1, 1)) * csm
-#show.imshow(abs(coil_images),tile_shape=(4,2))
-#
-#
-##%%
-##Undersample
-#reload(simulation)
-#acc_factor = 2
-#ref_lines = 16
-##kspace = transform.transform_image_to_kspace(coil_images,dim=(1,2))
-##kspace_full = kspace
-##kspace[:,1:matrix_size:acc_factor,:] = 0
-#(data,pat) = simulation.sample_data(phan,csm,acc_factor,ref_lines)
-#
-#noise = np.random.standard_normal(data.shape) + 1j*np.random.standard_normal(data.shape)
-#
-#kspace = np.logical_or(pat==1,pat==3).astype('float32')*(data + (2*noise/matrix_size))
-#alias_img = transform.transform_kspace_to_image(kspace,dim=(1,2)) * np.sqrt(acc_factor)
-#show.imshow(abs(alias_img))
-#
-##%%
-#reload(sense)
-#(unmix, gmap) = sense.calculate_sense_unmixing(acc_factor,csm)
-#show.imshow(abs(gmap))
+#%%
+reload(simulation)
+matrix_size = 256
+csm = simulation.generate_birdcage_sensitivities(matrix_size)
+phan = simulation.phantom(matrix_size)
+coil_images = np.tile(phan,(8, 1, 1)) * csm
+show.imshow(abs(coil_images),tile_shape=(4,2))
+
+
+#%%
+#Undersample
+reload(simulation)
+acc_factor = 2
+ref_lines = 16
+(data,pat) = simulation.sample_data(phan,csm,acc_factor,ref_lines)
+noise = np.random.standard_normal(data.shape) + 1j*np.random.standard_normal(data.shape)
+kspace = np.logical_or(pat==1,pat==3).astype('float32')*(data + (2*noise/matrix_size))
+alias_img = transform.transform_kspace_to_image(kspace,dim=(1,2)) * np.sqrt(acc_factor)
+show.imshow(abs(alias_img))
+
+#%%
+reload(sense)
+(unmix_sense, gmap_sense) = sense.calculate_sense_unmixing(acc_factor,csm)
+show.imshow(abs(gmap_sense))
+recon_sense = np.squeeze(np.sum(alias_img * unmix_sense,0))
+show.imshow(abs(recon_sense))
 
 #%%
 reload(grappa)
-#sp.io.savemat('raw_data.mat',{'kspace':kspace, 'pat': pat, 'csm': csm,'data':data})
-unmix = grappa.calculate_grappa_unmixing(data, acc_factor, data_mask=pat>1, csm=csm)
-#show.imshow(abs(gmap))
+(unmix_grappa,gmap_grappa) = grappa.calculate_grappa_unmixing(data, acc_factor, data_mask=pat>1, csm=csm)
+show.imshow(abs(gmap_grappa))
+recon_grappa = np.squeeze(np.sum(alias_img * unmix_sense,0))
+show.imshow(abs(recon_grappa))
 
-
-#%%
-# Unalias/Combine coils
-recon = np.squeeze(np.sum(alias_img * unmix,0))
-show.imshow(abs(recon))
