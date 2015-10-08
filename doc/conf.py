932f9928a2565726dd2587ae423e857133190e17
Gregory R. Lee <gregory.lee@cchmc.org>
2015-10-08 19:39:34 -0400

    update sphinx configuration to also support numpy docstrings

--- doc/conf.py
+++ doc/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.pngmath',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
